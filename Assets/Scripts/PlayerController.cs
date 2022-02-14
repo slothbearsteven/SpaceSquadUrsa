11a9645ca088c7b59f8ef037ad36e8573e70de8e
Steven Kennedy <umbrylax@gmail.com>
2022-02-14 10:27:17 -0700

    player movement successfully constrained

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private float speed = 10;
     public float horizontalInput;
     public float verticalInput;
+    private float xbounds = 22.0f;
+    private float zbounds = 11.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,23 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        if (transform.position.x <= -xbounds)
+        {
+            transform.position = new Vector3(-xbounds, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x >= xbounds)
+        {
+            transform.position = new Vector3(xbounds, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z <= -zbounds)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zbounds);
+        }
+        if (transform.position.z >= zbounds)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zbounds);
+        }
+
     }
 }
 
